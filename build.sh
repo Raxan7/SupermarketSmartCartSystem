0d9514518ff59956a413b96c2c1252d6a14f0c9a
Raxan7 <manyerere201@gmail.com>
2024-04-19 12:36:51 +0300

    Created build.sh

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ python3 manage.py migrate
 
 DJANGO_SUPERUSER_EMAIL="admin@gmail.com" \
 DJANGO_SUPERUSER_PASSWORD="admin" \
-python3 manage.py createsuperuser --email admin@gmail.com --noinput 
+python3 manage.py createsuperuser --username admin@gmail.com --noinput 
 
 python3 manage.py collectstatic --no-input
 
