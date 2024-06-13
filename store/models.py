dddfade9d178ccde7e8484c8a69e15329389acc1
Raxan7 <manyerere201@gmail.com>
2024-06-13 10:34:54 +0300

    Removed ImageField

--- store/models.py
+++ store/models.py
@@ -26,7 +26,6 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = CloudinaryField("image")
-    # image = models.ImageField(upload_to='products/')
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     stock_count = models.IntegerField()
 
